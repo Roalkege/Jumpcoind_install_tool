c31d21df312cf7e1e13a9a5e606513120466ff3f
Nick <nickm10@hotmail.de>
2019-02-18 18:37:40 +0100

    php script to check for running ./jumpcoind

---
+++ jumpcoind_check.php
@@ -0,0 +1,15 @@
+
+<?php
+
+        exec("./jumpcoind getblockcount", $output, $return_var);
+        $test = json_encode($output);
+        echo($test);
+        if($test == '[]'){
+                echo "ATTENTION";
+                exec("./jumpcoind");
+}
+        else{
+                echo "WALLET RUNNING!!!!!!!!!!!!";
+}
+?>
+
