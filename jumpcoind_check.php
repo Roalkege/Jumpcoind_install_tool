1bf3fec004aa08811d0ec3f5f5a26064a1aa3bbd
Roalkege <44238399+Roalkege@users.noreply.github.com>
2019-02-19 17:31:56 +0100

    Update jumpcoind_check.php

--- jumpcoind_check.php
+++ jumpcoind_check.php
@@ -1,15 +1,13 @@
-
 <?php
 
-        exec("./jumpcoind getblockcount", $output, $return_var);
-        $test = json_encode($output);
-        echo($test);
-        if($test == '[]'){
-                echo "ATTENTION";
-                exec("./jumpcoind");
+	exec("./jumpcoind getblockcount", $output, $return_var);
+	$test = json_encode($output);
+	echo($test);
+	if($test == '[]'){
+		echo "ATTENTION";
+		exec("./jumpcoind");
 }
-        else{
-                echo "WALLET RUNNING!!!!!!!!!!!!";
+	else{
+		echo "WALLET RUNNING!";
 }
 ?>
-
