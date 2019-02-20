585f4c199496cb1915bc4ccec4bd9dbcac9bb4f4
Roalkege <44238399+Roalkege@users.noreply.github.com>
2019-02-20 23:38:30 +0100

    Update jumpcoind_install.sh

--- jumpcoind_install.sh
+++ jumpcoind_install.sh
@@ -71,4 +71,4 @@ cd ~
 echo The Wallet is now starting and you wont see anything below, but the Wallet has started. Just close this terminal and open a new one.
 #execute jumpcoind
 ./jumpcoind -daemon
-
+exit n
